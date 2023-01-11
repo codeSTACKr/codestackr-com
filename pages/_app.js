c3c47c80442e99e1c29a4006e30b7014be71cb65
codeSTACKr <codestackr@gmail.com>
2023-01-10 18:52:14 -0600

    google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,20 @@
+import Script from 'next/script'
 import '../styles/globals.css'
 import "@code-hike/mdx/dist/index.css"
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script src="https://www.googletagmanager.com/gtag/js?id=UA-142379631-1" strategy="afterInteractive"></Script>
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-142379631-1');`}
+      </Script>
+
+      <Component {...pageProps} />
+    </>
+  )
 }
